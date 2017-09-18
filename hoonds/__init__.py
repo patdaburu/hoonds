af15e7b2a32eadd5cb8fd9f3dcd6322be3415ee4
patdaburu <pat@daburu.net>
2017-09-18 15:27:08 -0500

    Minor changes, plus revision update.

--- hoonds/__init__.py
+++ hoonds/__init__.py
@@ -5,8 +5,8 @@
 .. currentmodule:: __init__.py
 .. moduleauthor:: Pat Daburu <pat@daburu.net>
 
-hoonds is an unassuming  collection of tools for python.
+hoonds is an unassuming collection of tools for python.
 """
 
-__version__ = '0.0.4'  # the working version
-__release__ = '0.0.4'  # the release version
+__version__ = '0.0.5'  # the working version
+__release__ = '0.0.5'  # the release version
