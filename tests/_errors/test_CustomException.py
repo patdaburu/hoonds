2691964e00119efb657457defea07def240e0d26
patdaburu <pat@daburu.net>
2017-11-09 04:55:07 -0600

    Added errors module and CustomException.

---
+++ tests/_errors/test_CustomException.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from hoonds.errors import CustomException
+
+
+class TestCustomException(CustomException):
+    pass
+
+
+class TestGeometryExceptionSuite(unittest.TestCase):
+
+    def test_initWithoutInner_verify(self):
+        ge = TestCustomException(message='Test Message')
+        self.assertEqual('Test Message', ge.message)
+        self.assertIsNone(ge.inner)
+
+    def test_initWithInner_verify(self):
+        inner = Exception()
+        ge = TestCustomException(message='Test Message',
+                                 inner=inner)
+        self.assertEqual('Test Message', ge.message)
+        self.assertTrue(ge.inner == inner)
